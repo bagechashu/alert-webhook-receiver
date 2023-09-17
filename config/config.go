fea83aa35477815b2589ece6150bb14e1268abfa
lixiaoxue <lixiaoxue@example.com>
2023-09-17 17:36:51 +0400

    update: add godotenv

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type dingRobot struct {
 	Token  string
@@ -12,6 +16,13 @@ var (
 )
 
 func init() {
+	env := os.Getenv("ENV")
+	if "" == env {
+		env = "production"
+		godotenv.Load()
+	}
+	godotenv.Load(".env." + env)
+
 	DingRobot.Token = os.Getenv("DING_ROBOT_TOKEN")
 	DingRobot.Secret = os.Getenv("DING_ROBOT_SECRET")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.11
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
